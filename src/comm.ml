ed6755119dbe8d6291ec4ce21f7b6ec4829ae11b
hennequin <gje.hennequin@gmail.com>
2022-02-02 10:58:02 +0000

    separating out the mpi vs non-mpi versions

--- src/comm.ml
+++ src/comm.ml
@@ -1,53 +1,5 @@
 include Comm_typ
 
-(** {1 Single node} *)
-
-(** Communication on a single node -- trivial, but can be used as a fallback *)
-module Single (R : sig
-  val init_rng : int -> unit
-end) =
-struct
-  let n_nodes = 1
-  let barrier () = ()
-  let scatter x = x.(0)
-  let gather x = [| x |]
-
-  let gatheroption x =
-    Array.map
-      (function
-        | None -> failwith "gatheroption"
-        | Some z -> z)
-      x
-
-
-  let allgather x = [| x |]
-
-  let allgatheroption x =
-    Array.map
-      (function
-        | None -> failwith "allgatheroption"
-        | Some z -> z)
-      x
-
-
-  let broadcast x = x
-  let broadcast' f = f ()
-  let root_receive x (_ : int) = x
-  let send_to_root _ = ()
-  let rank = 0
-  let first = true
-  let root_perform f = f ()
-
-  let self_init_rng () =
-    Random.self_init ();
-    R.init_rng Random.(int 1000000000)
-
-
-  let with_same_rng f = f ()
-  let print = Printf.printf "%s%!"
-  let print_endline = Printf.printf "%s\n%!"
-end
-
 (** {1 Multi-node via the MPI library} *)
 
 let trans x = Array.(init (length x.(0)) (fun j -> init (length x) (fun i -> x.(j).(i))))
--- src/comm.mli
+++ src/comm.mli
@@ -1,9 +1,5 @@
 include module type of Comm_typ
 
-module Single (R : sig
-  val init_rng : int -> unit
-end) : T
-
 module Mpi (R : sig
   val init_rng : int -> unit
 end) : T
